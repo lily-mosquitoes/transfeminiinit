bdcb1813f804935ade5ab4aa72256bb3c8f39396
Lílian <lily.mosquitoes@gmail.com>
2022-01-16 21:02:47 +0200

    fixed card sizes

--- transfeminiinit/theme/static_src/tailwind.config.js
+++ transfeminiinit/theme/static_src/tailwind.config.js
@@ -69,5 +69,6 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/line-clamp'),
     ],
 }
