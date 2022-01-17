e39cea821ecaded76b6da1584ad7facb87fa61c6
Lílian <lily.mosquitoes@gmail.com>
2022-01-17 20:30:16 +0200

    created post filtering front and translations

--- transfeminiinit/website/templatetags/website_extras.py
+++ transfeminiinit/website/templatetags/website_extras.py
@@ -42,5 +42,21 @@ def get_language_list():
     return language_list
 
 @register.simple_tag
+def all_tags_filter():
+    return _('all_tags_filter')
+
+@register.simple_tag
+def time_filter_all():
+    return _('time_filter_all')
+
+@register.simple_tag
+def get_time_filters():
+    time_filters = [
+        _('time_filter_past_year'),
+        _('time_filter_recent'),
+    ]
+    return time_filters
+
+@register.simple_tag
 def get_verbose_field_name(instance, field_name):
     return instance._meta.get_field(field_name).verbose_name.title()
--- transfeminiinit/website/views.py
+++ transfeminiinit/website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.utils import translation
 from django.utils.translation import gettext_lazy as _
-from .models import Post
+from .models import Post, Tag
 from django.views import generic
 
 def home(request):
@@ -25,6 +25,12 @@ class PostListView(generic.ListView):
     model = Post
     template_name = 'post_list.html'
     context_object_name = 'post_list'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostListView, self).get_context_data(**kwargs)
+        context['tag_list'] = Tag.objects.all()
+        print(context)
+        return context
 
 class PostDetailView(generic.DetailView):
     model = Post
